3226bc0f175048a0fd3a0ceeaea6bff0d3d128ea
Newton <newtonlaw18@outlook.com>
2017-03-08 22:01:35 +0800

    Add contact page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
+  get '/contact' => 'pages#contact'
 end
